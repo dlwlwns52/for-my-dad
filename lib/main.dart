ed0f5097250ea4061737c4f11a5068a553b9d90b
jijun <dlwlwns52@naver.com>
2025-09-01 00:30:45 +0900

    Feat: fmd 프로젝트 시작

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fms/UIApplication/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(393, 852), // 피그마 기준
+      minTextAdapt: true, // 글자 크기 조정
+      splitScreenMode: true, // 앱 창 크기 변경시 자동 스케일링
+      builder: (context, child) {
+        return MaterialApp(title: 'FMD', home: const MainScreen());
+      },
+    );
+  }
+}
