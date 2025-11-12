70d1e148082d3e83a7d335a534f6e57c07e86bd1
jijun <dlwlwns52@naver.com>
2025-11-12 20:36:21 +0900

    Feat: 저장된 장소 View hive 연동 중

--- lib/store_location/store_location_view.dart
+++ lib/store_location/store_location_view.dart
@@ -1,148 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fms/Module/db/hive_Service.dart';
 import 'package:fms/constants/app_colors.dart';
+import 'package:fms/store_location/store_location_viewmodel.dart';
+import 'package:provider/provider.dart';
 
 class SavedPlacesView extends StatelessWidget {
   const SavedPlacesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final places = [
-      {'title': '비밀 장소 2', 'distance': '522m', 'date': '2025.11.08.'},
-      {'title': '비밀 장소 1', 'distance': '551m', 'date': '2025.11.08.'},
-    ];
-    return Scaffold(
-      backgroundColor: AppColors.storeSpotBackground,
-      appBar: AppBar(
-        backgroundColor: Color(0xFFFFFFFF),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Color(0xFF183317)),
-          onPressed: () => Navigator.pop(context),
-        ),
-        scrolledUnderElevation: 0,
-        centerTitle: true,
-        title: Text(
-          '저장된 장소',
-          style: TextStyle(
-            color: AppColors.midiumText,
-            fontWeight: FontWeight.w700,
-            fontSize: 20.sp,
-            fontFamily: 'Pretendard',
-          ),
-        ),
-      ),
-
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 18),
-        child: ListView.builder(
-          itemCount: places.length,
-          itemBuilder: (context, index) {
-            final place = places[index];
-            return Container(
-              margin: const EdgeInsets.only(bottom: 16),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(16),
-                border: Border.all(color: const Color(0xFFDBE5DB)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withValues(alpha: 0.05),
-                    blurRadius: 4,
-                    offset: const Offset(0, 2),
-                  ),
-                ],
+    return ChangeNotifierProvider(
+      create: (_) => StoreLocationViewmodel(SpotService())..init(),
+      builder: (context, _) {
+        return Scaffold(
+          backgroundColor: AppColors.storeSpotBackground,
+          appBar: AppBar(
+            backgroundColor: Color(0xFFFFFFFF),
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Color(0xFF183317)),
+              onPressed: () => Navigator.pop(context),
+            ),
+            scrolledUnderElevation: 0,
+            centerTitle: true,
+            title: Text(
+              '저장된 장소',
+              style: TextStyle(
+                color: AppColors.midiumText,
+                fontWeight: FontWeight.w700,
+                fontSize: 20.sp,
+                fontFamily: 'Pretendard',
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // 제목과 버튼들
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            ),
+          ),
+
+          body: Consumer<StoreLocationViewmodel>(
+            builder: (context, vm, _) {
+              if (vm.isLoading) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                ); //로딩중일때 TODO: 추후에 디자인 적용
+              }
+              if (vm.error != null) {
+                //에러발생시 TODO: 추후에 디자인 적용
+                return Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
                       children: [
                         Text(
-                          place['title']!,
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w700,
-                            color: Color(0xFF183317),
-                          ),
+                          '불러오기 실패\n${vm.error}',
+                          textAlign: TextAlign.center,
                         ),
-                        Row(
-                          children: [
-                            _iconButton(
-                              icon: Icons.map,
-                              borderColor: const Color(0xFF2468E2),
-                              iconColor: const Color(0xFF2468E2),
-                              onPressed: () {},
-                            ),
-                            const SizedBox(width: 8),
-                            _iconButton(
-                              icon: Icons.navigation_outlined,
-                              borderColor: const Color(0xFF275025),
-                              iconColor: const Color(0xFF275025),
-                              onPressed: () {},
-                            ),
-                            const SizedBox(width: 8),
-                            _iconButton(
-                              icon: Icons.delete_outline,
-                              borderColor: const Color(0xFFD73B3B),
-                              iconColor: const Color(0xFFD73B3B),
-                              onPressed: () {},
-                            ),
-                          ],
+                        const SizedBox(height: 12),
+                        ElevatedButton(
+                          onPressed: vm.loadSpots,
+                          child: const Text('다시 시도'),
                         ),
                       ],
                     ),
-                    const SizedBox(height: 12),
-                    Text.rich(
-                      TextSpan(
-                        text: '거리: ',
-                        style: TextStyle(
-                          color: Color(0xFF647A60),
-                          fontWeight: FontWeight.w500,
-                          fontSize: 14.sp,
-                        ),
-                        children: [
-                          TextSpan(
-                            text: place['distance'],
-                            style: const TextStyle(
-                              fontWeight: FontWeight.w700,
-                              color: Color(0xFF183317),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 4),
-                    Text.rich(
-                      TextSpan(
-                        text: '저장: ',
-                        style: TextStyle(
-                          color: Color(0xFF647A60),
-                          fontWeight: FontWeight.w500,
-                          fontSize: 14.sp,
-                        ),
-                        children: [
-                          TextSpan(
-                            text: place['date'],
-                            style: const TextStyle(
-                              color: Color(0xFF183317),
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    //사진
+                  ),
+                );
+              }
+              // if (vm.spots.isEmpty) {
+              return RefreshIndicator(
+                onRefresh: vm.loadSpots,
+                child: ListView(
+                  children: const [
+                    SizedBox(height: 160),
+                    Center(child: Text('저장된 장소가 없습니다.')),
                   ],
                 ),
-              ),
-            );
-          },
-        ),
-      ),
+              );
+              // }
+              // return Padding(
+              //   padding: const EdgeInsets.symmetric(
+              //     horizontal: 16,
+              //     vertical: 18,
+              //   ),
+              // );
+            },
+          ),
+        );
+      },
     );
   }
 
@@ -167,3 +108,119 @@ class SavedPlacesView extends StatelessWidget {
     );
   }
 }
+
+
+
+          // Padding(
+          //   padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 18),
+          //   child: ListView.builder(
+          //     itemCount: places.length,
+          //     itemBuilder: (context, index) {
+          //       final place = places[index];
+          //       return Container(
+          //         margin: const EdgeInsets.only(bottom: 16),
+          //         decoration: BoxDecoration(
+          //           color: Colors.white,
+          //           borderRadius: BorderRadius.circular(16),
+          //           border: Border.all(color: const Color(0xFFDBE5DB)),
+          //           boxShadow: [
+          //             BoxShadow(
+          //               color: Colors.black.withValues(alpha: 0.05),
+          //               blurRadius: 4,
+          //               offset: const Offset(0, 2),
+          //             ),
+          //           ],
+          //         ),
+          //         child: Padding(
+          //           padding: const EdgeInsets.all(16),
+          //           child: Column(
+          //             crossAxisAlignment: CrossAxisAlignment.start,
+          //             children: [
+          //               // 제목과 버튼들
+          //               Row(
+          //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //                 children: [
+          //                   Text(
+          //                     place['title']!,
+          //                     style: const TextStyle(
+          //                       fontSize: 18,
+          //                       fontWeight: FontWeight.w700,
+          //                       color: Color(0xFF183317),
+          //                     ),
+          //                   ),
+          //                   Row(
+          //                     children: [
+          //                       _iconButton(
+          //                         icon: Icons.map,
+          //                         borderColor: const Color(0xFF2468E2),
+          //                         iconColor: const Color(0xFF2468E2),
+          //                         onPressed: () {},
+          //                       ),
+          //                       const SizedBox(width: 8),
+          //                       _iconButton(
+          //                         icon: Icons.navigation_outlined,
+          //                         borderColor: const Color(0xFF275025),
+          //                         iconColor: const Color(0xFF275025),
+          //                         onPressed: () {},
+          //                       ),
+          //                       const SizedBox(width: 8),
+          //                       _iconButton(
+          //                         icon: Icons.delete_outline,
+          //                         borderColor: const Color(0xFFD73B3B),
+          //                         iconColor: const Color(0xFFD73B3B),
+          //                         onPressed: () {},
+          //                       ),
+          //                     ],
+          //                   ),
+          //                 ],
+          //               ),
+          //               const SizedBox(height: 12),
+          //               Text.rich(
+          //                 TextSpan(
+          //                   text: '거리: ',
+          //                   style: TextStyle(
+          //                     color: Color(0xFF647A60),
+          //                     fontWeight: FontWeight.w500,
+          //                     fontSize: 14.sp,
+          //                   ),
+          //                   children: [
+          //                     TextSpan(
+          //                       text: place['distance'],
+          //                       style: const TextStyle(
+          //                         fontWeight: FontWeight.w700,
+          //                         color: Color(0xFF183317),
+          //                       ),
+          //                     ),
+          //                   ],
+          //                 ),
+          //               ),
+          //               const SizedBox(height: 4),
+          //               Text.rich(
+          //                 TextSpan(
+          //                   text: '저장: ',
+          //                   style: TextStyle(
+          //                     color: Color(0xFF647A60),
+          //                     fontWeight: FontWeight.w500,
+          //                     fontSize: 14.sp,
+          //                   ),
+          //                   children: [
+          //                     TextSpan(
+          //                       text: place['date'],
+          //                       style: const TextStyle(
+          //                         color: Color(0xFF183317),
+          //                         fontWeight: FontWeight.w700,
+          //                       ),
+          //                     ),
+          //                   ],
+          //                 ),
+          //               ),
+
+          //               //사진
+          //               // memo
+          //             ],
+          //           ),
+          //         ),
+          //       );
+          //     },
+          //   ),
+          // ),
