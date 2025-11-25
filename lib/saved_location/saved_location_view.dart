b533d7d3d7626d9dee9f2678507594644d3a2201
jijun <dlwlwns52@naver.com>
2025-11-25 23:23:42 +0900

    Feat: 거리 1000m 이상은 km, 미만은 m 단위로 표시하도록 변경

--- lib/saved_location/saved_location_view.dart
+++ lib/saved_location/saved_location_view.dart
@@ -1,21 +1,25 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fms/Module/db/hive_Service.dart';
 import 'package:fms/Module/utils/logger.dart';
+import 'package:fms/StoreLocationViewmodel.dart';
 import 'package:fms/UIApplication/main_screen.dart';
 import 'package:fms/constants/app_colors.dart';
-import 'package:fms/store_location/store_location_viewmodel.dart';
+import 'package:fms/saved_location/saved_location_viewmodel.dart';
 import 'package:provider/provider.dart';
 
-class SavedPlacesView extends StatelessWidget {
-  const SavedPlacesView({super.key});
+class SavedLocationsView extends StatelessWidget {
+  const SavedLocationsView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (_) => StoreLocationViewmodel(SpotService())..init(),
+      create: (_) =>
+          SavedLocationViewmodel(SpotService())..getCurrentPosition(),
       builder: (context, _) {
+        final savedVM = context.watch<SavedLocationViewmodel>();
         return Scaffold(
           backgroundColor: AppColors.storeSpotBackground,
           appBar: AppBar(
@@ -191,6 +195,114 @@ class SavedPlacesView extends StatelessWidget {
                   horizontal: 16,
                   vertical: 18,
                 ),
+                child: ListView.builder(
+                  itemCount: vm.spots.length,
+                  itemBuilder: (context, index) {
+                    final spot = vm.spots[index];
+                    final distance = savedVM.calculateDistance(spot);
+                    return Container(
+                      margin: const EdgeInsets.only(bottom: 16),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                        border: Border.all(color: const Color(0xFFDBE5DB)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withValues(alpha: 0.05),
+                            blurRadius: 4,
+                            offset: const Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // 제목과 버튼들
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  spot.placeName,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w700,
+                                    color: Color(0xFF183317),
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    _iconButton(
+                                      icon: Icons.map,
+                                      borderColor: const Color(0xFF2468E2),
+                                      iconColor: const Color(0xFF2468E2),
+                                      onPressed: () {},
+                                    ),
+                                    const SizedBox(width: 8),
+                                    _iconButton(
+                                      icon: Icons.navigation_outlined,
+                                      borderColor: const Color(0xFF275025),
+                                      iconColor: const Color(0xFF275025),
+                                      onPressed: () {},
+                                    ),
+                                    const SizedBox(width: 8),
+                                    _iconButton(
+                                      icon: Icons.delete_outline,
+                                      borderColor: const Color(0xFFD73B3B),
+                                      iconColor: const Color(0xFFD73B3B),
+                                      onPressed: () {},
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 12),
+                            Text.rich(
+                              TextSpan(
+                                text: '거리: ',
+                                style: TextStyle(
+                                  color: Color(0xFF647A60),
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 14.sp,
+                                ),
+                                children: [
+                                  TextSpan(
+                                    text: distance,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.w700,
+                                      color: Color(0xFF183317),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(height: 4),
+                            Text.rich(
+                              TextSpan(
+                                text: '저장: ',
+                                style: TextStyle(
+                                  color: Color(0xFF647A60),
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 14.sp,
+                                ),
+                                children: [
+                                  TextSpan(
+                                    text: vm.formatDate(spot.createdAt),
+                                    style: const TextStyle(
+                                      color: Color(0xFF183317),
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
               );
             },
           ),
@@ -199,140 +311,24 @@ class SavedPlacesView extends StatelessWidget {
     );
   }
 
-  //   Widget _iconButton({
-  //     required IconData icon,
-  //     required Color borderColor,
-  //     required Color iconColor,
-  //     required VoidCallback onPressed,
-  //   }) {
-  //     return Container(
-  //       width: 36,
-  //       height: 36,
-  //       decoration: BoxDecoration(
-  //         border: Border.all(color: borderColor, width: 1.0),
-  //         borderRadius: BorderRadius.circular(8),
-  //       ),
-  //       child: IconButton(
-  //         padding: EdgeInsets.zero,
-  //         icon: Icon(icon, color: iconColor, size: 20),
-  //         onPressed: onPressed,
-  //       ),
-  //     );
-  //   }
+  Widget _iconButton({
+    required IconData icon,
+    required Color borderColor,
+    required Color iconColor,
+    required VoidCallback onPressed,
+  }) {
+    return Container(
+      width: 36,
+      height: 36,
+      decoration: BoxDecoration(
+        border: Border.all(color: borderColor, width: 1.0),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: IconButton(
+        padding: EdgeInsets.zero,
+        icon: Icon(icon, color: iconColor, size: 20),
+        onPressed: onPressed,
+      ),
+    );
+  }
 }
-
-
-
-          // Padding(
-          //   padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 18),
-          //   child: ListView.builder(
-          //     itemCount: places.length,
-          //     itemBuilder: (context, index) {
-          //       final place = places[index];
-          //       return Container(
-          //         margin: const EdgeInsets.only(bottom: 16),
-          //         decoration: BoxDecoration(
-          //           color: Colors.white,
-          //           borderRadius: BorderRadius.circular(16),
-          //           border: Border.all(color: const Color(0xFFDBE5DB)),
-          //           boxShadow: [
-          //             BoxShadow(
-          //               color: Colors.black.withValues(alpha: 0.05),
-          //               blurRadius: 4,
-          //               offset: const Offset(0, 2),
-          //             ),
-          //           ],
-          //         ),
-          //         child: Padding(
-          //           padding: const EdgeInsets.all(16),
-          //           child: Column(
-          //             crossAxisAlignment: CrossAxisAlignment.start,
-          //             children: [
-          //               // 제목과 버튼들
-          //               Row(
-          //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //                 children: [
-          //                   Text(
-          //                     place['title']!,
-          //                     style: const TextStyle(
-          //                       fontSize: 18,
-          //                       fontWeight: FontWeight.w700,
-          //                       color: Color(0xFF183317),
-          //                     ),
-          //                   ),
-          //                   Row(
-          //                     children: [
-          //                       _iconButton(
-          //                         icon: Icons.map,
-          //                         borderColor: const Color(0xFF2468E2),
-          //                         iconColor: const Color(0xFF2468E2),
-          //                         onPressed: () {},
-          //                       ),
-          //                       const SizedBox(width: 8),
-          //                       _iconButton(
-          //                         icon: Icons.navigation_outlined,
-          //                         borderColor: const Color(0xFF275025),
-          //                         iconColor: const Color(0xFF275025),
-          //                         onPressed: () {},
-          //                       ),
-          //                       const SizedBox(width: 8),
-          //                       _iconButton(
-          //                         icon: Icons.delete_outline,
-          //                         borderColor: const Color(0xFFD73B3B),
-          //                         iconColor: const Color(0xFFD73B3B),
-          //                         onPressed: () {},
-          //                       ),
-          //                     ],
-          //                   ),
-          //                 ],
-          //               ),
-          //               const SizedBox(height: 12),
-          //               Text.rich(
-          //                 TextSpan(
-          //                   text: '거리: ',
-          //                   style: TextStyle(
-          //                     color: Color(0xFF647A60),
-          //                     fontWeight: FontWeight.w500,
-          //                     fontSize: 14.sp,
-          //                   ),
-          //                   children: [
-          //                     TextSpan(
-          //                       text: place['distance'],
-          //                       style: const TextStyle(
-          //                         fontWeight: FontWeight.w700,
-          //                         color: Color(0xFF183317),
-          //                       ),
-          //                     ),
-          //                   ],
-          //                 ),
-          //               ),
-          //               const SizedBox(height: 4),
-          //               Text.rich(
-          //                 TextSpan(
-          //                   text: '저장: ',
-          //                   style: TextStyle(
-          //                     color: Color(0xFF647A60),
-          //                     fontWeight: FontWeight.w500,
-          //                     fontSize: 14.sp,
-          //                   ),
-          //                   children: [
-          //                     TextSpan(
-          //                       text: place['date'],
-          //                       style: const TextStyle(
-          //                         color: Color(0xFF183317),
-          //                         fontWeight: FontWeight.w700,
-          //                       ),
-          //                     ),
-          //                   ],
-          //                 ),
-          //               ),
-
-          //               //사진
-          //               // memo
-          //             ],
-          //           ),
-          //         ),
-          //       );
-          //     },
-          //   ),
-          // ),
--- lib/saved_location/saved_location_viewmodel.dart
+++ lib/saved_location/saved_location_viewmodel.dart
@@ -2,51 +2,62 @@
 import 'package:flutter/foundation.dart';
 import 'package:fms/Module/db/hive_Service.dart';
 import 'package:fms/Module/db/hive_model.dart';
-import 'package:fms/Module/utils/logger.dart';
+import 'package:geolocator/geolocator.dart';
 // import 'package:geolocator/geolocator.dart';
 // import 'package:image_picker/image_picker.dart';
 
-class StoreLocationViewmodel extends ChangeNotifier {
+class SavedLocationViewmodel extends ChangeNotifier {
   final SpotService _spotService;
 
-  StoreLocationViewmodel(this._spotService);
+  SavedLocationViewmodel(this._spotService);
 
-  bool isLoading = false;
-  String? error;
-  List<Spot> spots = [];
+  Position? currentPosition;
 
-  Future<void> init() async {
-    await loadSpots();
-  }
-
-  Future<void> loadSpots() async {
-    isLoading = true;
-    error = null;
-    notifyListeners();
-
-    try {
-      spots = await _spotService.fetchAllSpots();
-      isLoading = false;
-      notifyListeners();
-    } catch (e) {
-      error = e.toString();
-      printDebug("loadSpots 에러 : $e");
-    } finally {
-      isLoading = false;
-      printDebug("loadSpots 로딩 실패");
-      notifyListeners();
+  //MARK: 현재 위치 가져오기
+  Future<void> getCurrentPosition() async {
+    //MARK: 권한 체크 & 요청
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      //MARK: GPS 자체가 꺼져 있음
+      throw Exception("위치 서비스가 꺼져 있습니다.");
     }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception("위치 권한이 거부되었습니다.");
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception("위치 권한이 영구적으로 거부되었습니다. 설정에서 권한을 허용해야 합니다.");
+    }
+
+    //MARK: 현재 위치 가져오기
+    currentPosition = await Geolocator.getCurrentPosition(
+      locationSettings: const LocationSettings(accuracy: LocationAccuracy.best),
+    );
+    debugPrint(
+      "현재 위치(saved): 위도 ${currentPosition?.latitude}, 경도 ${currentPosition?.longitude}",
+    );
+    notifyListeners();
   }
 
-  Future<void> deleteAt(int index) async {
-    try {
-      await _spotService.deleteSpot(index);
-      spots.removeAt(index);
-      notifyListeners();
-    } catch (e) {
-      error = '삭제 실패: $e';
-      printDebug("deleteAt 에러 : $e");
-      notifyListeners();
+  String calculateDistance(Spot spot) {
+    if (currentPosition == null) return "이전 화면으로 갔다가 다시 들어오셈";
+
+    final distance = Geolocator.distanceBetween(
+      currentPosition!.latitude,
+      currentPosition!.longitude,
+      spot.latitude,
+      spot.longitude,
+    );
+
+    if (distance < 1000) {
+      return "${distance.toStringAsFixed(1)}m";
+    } else {
+      return "${(distance / 1000).toStringAsFixed(1)}km";
     }
   }
 }
