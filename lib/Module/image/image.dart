56ea9753dc4b975e503c3e03cb8812ab913ed1e3
jijun <dlwlwns52@naver.com>
2025-12-08 21:28:39 +0900

    Feat: 이미지 전체화면 UI 좌 -> 우 스와이프시 뒤로가기 기능 적용

--- lib/Module/image/image.dart
+++ lib/Module/image/image.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:fms/Module/utils/swipe_back_detector.dart';
 
 class FullScreenImageView extends StatelessWidget {
   final String imagePath;
@@ -11,22 +12,24 @@ class FullScreenImageView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          Center(
-            child: InteractiveViewer(
-              child: Image.file(File(imagePath), fit: BoxFit.contain),
+      body: SwipeBackDetector(
+        child: Stack(
+          children: [
+            Center(
+              child: InteractiveViewer(
+                child: Image.file(File(imagePath), fit: BoxFit.contain),
+              ),
             ),
-          ),
-          Positioned(
-            top: 50,
-            right: 20,
-            child: IconButton(
-              icon: const Icon(Icons.close, color: Colors.white, size: 30),
-              onPressed: () => Navigator.pop(context),
+            Positioned(
+              top: 50,
+              right: 20,
+              child: IconButton(
+                icon: const Icon(Icons.close, color: Colors.white, size: 30),
+                onPressed: () => Navigator.pop(context),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
